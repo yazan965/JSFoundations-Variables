3d683d915c283772fd4307e2b40b8b35ac13b8b2
yazan965 <yazan.shweikeh@gmail.com>
2020-10-18 20:04:52 -0700

    Vardone

---
+++ story.js
@@ -0,0 +1,6 @@
+let username = "Mshary";
+let age = 25;
+let hobby ="Meditation";
+
+
+console.log(`"Mr./Mrs. ${username}, in 33 years, you will be ${age + 33} years old. By then you'll have decades of ${hobby} practice"`)
